275f9779a59bdaca1273b94c608fe927a6446e21
T.Okuda <t.okuda@keio.jp>
2021-05-09 07:42:53 +0900

    commit version 0.0.5 * Add useful samples * Organize functions to three main classes:   * AugmentImg   * DatasetCreator   * TfrecordConverter * modify documentations

--- tests/test_tfaug_tool.py
+++ tests/test_tfaug_tool.py
@@ -13,28 +13,31 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 
 
-tfexample_format = {"image":tf.io.FixedLenFeature([], dtype=tf.string),
-                    "msk":tf.io.FixedLenFeature([], dtype=tf.string)}
+tfexample_format = {"image": tf.io.FixedLenFeature([], dtype=tf.string),
+                    "msk": tf.io.FixedLenFeature([], dtype=tf.string)}
 
 
 def plot_dsresult(dataset, batch_size, num_batch, path_fig):
-    
-    fig, axs=plt.subplots(num_batch,batch_size, figsize=(batch_size,num_batch), dpi=200)
+
+    fig, axs = plt.subplots(num_batch, batch_size,
+                            figsize=(batch_size, num_batch), dpi=200)
     fig.suptitle(path_fig)
     for i, (im, lb) in enumerate(dataset):
-        
+
         for batch in range(batch_size):
-            axs[i,batch].axis("off")
-            axs[i,batch].imshow(im[0])
-        
+            axs[i, batch].axis("off")
+            axs[i, batch].imshow(im[0])
+
     plt.savefig(path_fig)
 
 
 def _dtype_to_tensor_spec(v):
-  return tf.TensorSpec(None, v) if isinstance(v, tf.dtypes.DType) else v
+    return tf.TensorSpec(None, v) if isinstance(v, tf.dtypes.DType) else v
+
 
 def _tensor_spec_to_dtype(v):
-  return v.dtype if isinstance(v, tf.TensorSpec) else v
+    return v.dtype if isinstance(v, tf.TensorSpec) else v
+
 
 def new_py_function(func, inp, Tout, name=None):
     def wrapped_func(*flat_inp):
@@ -44,15 +47,14 @@ def new_py_function(func, inp, Tout, name=None):
         return tf.nest.flatten(out, expand_composites=True)
     flat_Tout = tf.nest.flatten(Tout, expand_composites=True)
     flat_out = tf.py_function(
-        func=wrapped_func, 
+        func=wrapped_func,
         inp=tf.nest.flatten(inp, expand_composites=True),
         Tout=[_tensor_spec_to_dtype(v) for v in flat_Tout],
         name=name)
-    spec_out = tf.nest.map_structure(_dtype_to_tensor_spec, Tout, 
+    spec_out = tf.nest.map_structure(_dtype_to_tensor_spec, Tout,
                                      expand_composites=True)
     out = tf.nest.pack_sequence_as(spec_out, flat_out, expand_composites=True)
     return out
-
 
 
 def np_to_pngstr(npary):
@@ -61,11 +63,14 @@ def np_to_pngstr(npary):
         stimg = output.getvalue()
     return stimg
 
+
 def _bytes_feature(value):
     """Returns a bytes_list from a string / byte."""
     if isinstance(value, type(tf.constant(0))):
-      value = value.numpy() # BytesList won't unpack a string from an EagerTensor.
+        # BytesList won't unpack a string from an EagerTensor.
+        value = value.numpy()
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
 
 def read_imgs(path_imgs, mode='RGB'):
     ret_imgs = []
